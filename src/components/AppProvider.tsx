2bea469e0ec97cab23ada07ca56183b283348521
Chai Phonbopit <phonbopit@gmail.com>
2022-11-05 21:30:57 +0700

    Update naming for next env local

--- src/components/AppProvider.tsx
+++ src/components/AppProvider.tsx
@@ -7,7 +7,7 @@ import { Provider } from 'urql'
 
 import { graphqlClient } from '../services/lens-api'
 
-const apiKey = process.env.ALCHEMY_ID
+const apiKey = process.env.NEXT_PUBLIC_ALCHEMY_ID
 
 export const { chains, provider } = configureChains(
   [
